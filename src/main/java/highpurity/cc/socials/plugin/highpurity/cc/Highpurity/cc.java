d213ee2f60bc3f15f4255ece7a0ef3ce7c23c245
Highpurity <100718255+Highpurity@users.noreply.github.com>
2022-07-27 16:22:24 -0400

    config work

--- src/main/java/highpurity/cc/socials/plugin/highpurity/cc/Highpurity/cc.java
+++ src/main/java/highpurity/cc/socials/plugin/highpurity/cc/Highpurity/cc.java
@@ -2,6 +2,8 @@ package highpurity.cc.socials.plugin.highpurity.cc.Highpurity;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public final class cc extends JavaPlugin {
 
     @Override
@@ -11,7 +13,12 @@ public final class cc extends JavaPlugin {
         var logger = getLogger();
         logger.info("Starting SocialsPlugin!");
 
+        
+
         if()
+        logger.warning("This plugin will function properly with no configuration, however it is greatly encouraged to visit the config.yml file! Disable this warning in the config.yml file!");
+
+
 
 
     }
