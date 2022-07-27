c829f28fde1f11d629ace8ff2bb0e9f269f772e4
Highpurity <100718255+Highpurity@users.noreply.github.com>
2022-07-27 16:14:20 -0400

    Init

---
+++ src/main/java/highpurity/cc/socials/plugin/highpurity/cc/Highpurity/cc.java
@@ -0,0 +1,18 @@
+package highpurity.cc.socials.plugin.highpurity.cc.Highpurity;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class cc extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        var logger = getLogger();
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
