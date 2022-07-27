46e5637bf2aaa12c7eccc051771e1872991be67e
Highpurity <100718255+Highpurity@users.noreply.github.com>
2022-07-27 16:59:07 -0400

    Started work on social storing method

--- src/main/java/highpurity/cc/socials/plugin/highpurity/cc/Highpurity/Commands/Discord.java
+++ src/main/java/highpurity/cc/socials/plugin/highpurity/cc/Highpurity/Commands/Discord.java
@@ -1,6 +1,7 @@
 package highpurity.cc.socials.plugin.highpurity.cc.Highpurity.Commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,10 +15,17 @@ public class Discord implements CommandExecutor {
         if (sender instanceof Player p) {
             if (args.length == 1) {
                 var user = Bukkit.getPlayer(args[0]);
+                var display = user.getDisplayName();
                 if(user != null){
-                    
+                    p.sendMessage(ChatColor.YELLOW + display + ChatColor.WHITE + "'s Discord:");
+                    p.sendMessage("Discord!");
                 }
 
+            }else-if(args.length == 2){
+                p.getUniqueId();
+                
+
+                var social = args[1];
             }
         }
 
